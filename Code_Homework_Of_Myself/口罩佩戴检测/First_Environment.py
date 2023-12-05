e0b3c0f747a6742c0209e1540d8390aa53ce4512
djwll <1564474481@qq.com>
2023-12-05 15:01:00 +0800

    口罩检测_2023_12_5

---
+++ Code_Homework_Of_Myself/口罩佩戴检测/First_Environment.py
@@ -0,0 +1,22 @@
+import warnings
+# 忽视警告
+warnings.filterwarnings('ignore')
+
+import cv2
+from PIL import Image
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
+from torch.utils.data import DataLoader
+
+
+from torch_py.Utils import plot_image
+from torch_py.MTCNN.detector import FaceDetector
+from torch_py.MobileNetV1 import MobileNetV1
+from torch_py.FaceRec import Recognition
